afada83c20aac0da6d992e3d780746b6604ac075
Andrey Fits <andreyfits@gmail.com>
2022-01-24 22:53:52 +0300

    fix: adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,22 +6,17 @@ console.log(
     "Интерактивность, реализуемая через css +20\n"
 )
 
-const toggle = document.querySelector(".toggle");
-const nav = document.querySelector(".nav");
-const navList = document.querySelector(".nav-list");
-const navLinks = document.querySelectorAll(".nav-link");
-const show = document.querySelector('.show')
+const toggle = document.querySelector('.toggle');
+const navMenu = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
+const show = document.querySelector('.show');
 
 const toggleMenu = (event) => {
-    if (window.innerWidth <= 768) {
-        toggle.classList.toggle("open");
-        nav.classList.toggle("open");
-        navList.classList.toggle("open");
-        show.classList.toggle("hide")
-    }
+    toggle.classList.toggle('open');
+    navMenu.classList.toggle('open');
+    show.classList.toggle('hide');
 }
 
-toggle.addEventListener("click", toggleMenu)
-show.addEventListener("click", toggleMenu)
+toggle.addEventListener('click', toggleMenu)
 
-navLinks.forEach((element) => element.addEventListener("click", toggleMenu));
+navLinks.forEach((element) => element.addEventListener('click', toggleMenu));
