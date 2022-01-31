f9c7603ea89371472ab43515e766f5578d6f071b
Andrey Fits <andreyfits@gmail.com>
2022-01-31 21:47:44 +0300

    feat: add functionality for change images in section portfolio

---
+++ portfolio/assets/js/variables.js
@@ -0,0 +1,19 @@
+const variable = {
+    'blackout': document.querySelector('body'),
+    'navMenu': document.querySelector('.nav'),
+    'toggle': document.querySelector('.toggle'),
+    'switch-lang': document.querySelector('.switch-lang'),
+    'lng': document.querySelectorAll('.lng'),
+    'section-title': document.querySelectorAll('.section-title'),
+    'skills-container': document.querySelector('.skills-container'),
+    'portfolio-container': document.querySelector('.portfolio-container'),
+    'portfolio-buttons': document.querySelector('.portfolio-buttons'),
+    'portfolio-button': document.querySelectorAll('.portfolio-button'),
+    'portfolio-img': document.querySelectorAll('.portfolio-img'),
+    'seasons': ['winter', 'spring', 'summer', 'autumn'],
+    'data-theme': document.querySelectorAll('[data-theme]'),
+    'data-form': document.querySelectorAll('[data-form]'),
+    'data-i18n': document.querySelectorAll('[data-i18n]'),
+    'theme-change': document.querySelector('.theme-change'),
+}
+export default variable;
--- portfolio/index.js
+++ portfolio/index.js
@@ -1,46 +1,80 @@
-let requirements = `Итоговая оценка: 85/85.
-1. Вёрстка соответствует макету. Ширина экрана 768px +48
-  + блок <header> +6
-  + секция hero +6
-  + секция skills +6
-  + секция portfolio +6
-  + секция video +6
-  + секция price +6
-  + секция contacts +6
-  + блок <footer> +6
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15
-  + нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5
-  + нет полосы прокрутки при ширине страницы от 768рх до 480рх +5
-  + нет полосы прокрутки при ширине страницы от 480рх до 320рх +5
-3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22
-  + при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2
-  + при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4
-  + высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4
-  + при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4
-  + бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2
-  + ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2
-  + при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4
+let requirements = `Итоговая оценка: 25/85.
+1. Смена изображений в секции portfolio +25
+Изображения разных времён года получаем из папок с соответствующими названиями
+Изображения заменены на другие с целью улучшения качества созданного приложения
+  - при кликах по кнопкам Winter, Spring, Summer, Autumn в секции portfolio отображаются изображения из папки с соответствующим названием +20
+  - кнопка, по которой кликнули, становится активной т.е. выделяется стилем. Другие кнопки при этом будут неактивными +5
 `;
 
 console.log(requirements);
 
-const toggle = document.querySelector('.toggle');
-const navMenu = document.querySelector('nav');
-const blackout = document.querySelector('body');
+import variable from './assets/js/variables.js';
 
+/*======================================== LOCAL STORAGE ========================================*/
+let timeOfYear = 'autumn';
+
+const setLocalStorage = () => {
+    localStorage.setItem('season', timeOfYear);
+};
+
+const getLocalStorage = () => {
+    if (localStorage.getItem('season')) {
+        changeImage(localStorage.getItem('season'));
+    } else {
+        setLocalStorage();
+        getLocalStorage();
+    }
+};
+
+window.addEventListener('load', getLocalStorage);
+window.addEventListener('beforeunload', setLocalStorage);
+
+/*======================================== TOGGLE MENU ========================================*/
 function toggleMenu() {
-    toggle.classList.toggle('open');
-    navMenu.classList.toggle('open');
-    blackout.classList.toggle('open');
+    variable['toggle'].classList.toggle('open');
+    variable['navMenu'].classList.toggle('open');
+    variable['blackout'].classList.toggle('open');
 }
-
-toggle.addEventListener('click', toggleMenu)
-navMenu.addEventListener('click', closeMenu)
 
 function closeMenu(event) {
     if (event.target.classList.contains('nav-link')) {
-        toggle.classList.remove('open');
-        navMenu.classList.remove('open');
-        blackout.classList.remove('open');
+        variable['toggle'].classList.remove('open');
+        variable['navMenu'].classList.remove('open');
+        variable['blackout'].classList.remove('open');
     }
 }
+
+/*============================== PORTFOLIO BUTTONS AND CHANGE IMAGES ==============================*/
+const makeActive = (elements, value, data) => {
+    elements.forEach(element => {
+        element.classList.remove('active');
+        if (element.dataset[data] === value) {
+            element.classList.add('active');
+        }
+    });
+};
+
+const changeImage = (season) => {
+    makeActive(variable['portfolio-button'], season, 'season');
+    variable['portfolio-img'].forEach((img, index) => img.src = `assets/img/${season}/${index + 1}.jpg`);
+    timeOfYear = season;
+};
+
+const changeImageEvent = (event) => {
+    if (event.target.classList.contains('portfolio-button')) {
+        changeImage(event.target.dataset.season);
+    }
+};
+
+/*======================================== CACHING IMAGES ========================================*/
+const preloadImages = (season) => {
+    for (let i = 1; i <= 6; i++) {
+        const img = new Image();
+        img.src = `assets/img/${season}/${i}.jpg`;
+    }
+};
+
+variable['seasons'].forEach(el => preloadImages(el));
+variable['portfolio-buttons'].addEventListener('click', changeImageEvent);
+variable['toggle'].addEventListener('click', toggleMenu);
+variable['navMenu'].addEventListener('click', closeMenu);
